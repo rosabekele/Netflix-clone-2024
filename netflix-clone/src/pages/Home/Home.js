064d7b9efe10099eee3a26f16da0e8403ed2f370
rosabekele <rbekele208@gmail.com>
2024-11-11 16:45:18 +0300

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner'
 const Home = () => {
   return (
     <>
         <Header/>
+        <Banner/>
         <Footer/> 
     </>
   )
